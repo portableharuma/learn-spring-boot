890f26ed10555a20b1f96a7b406648f62009ab5d
portableharuma <haruma.cheung@gmail.com>
2026-02-07 14:55:01 -0600

    Controller for courses completed. Shows just how easy it is to make a web application with Spring Boot

---
+++ src/main/java/com/haruma/springboot/learn_spring_boot/Course.java
@@ -0,0 +1,30 @@
+package com.haruma.springboot.learn_spring_boot;
+
+public class Course {
+    private long id;
+    private String name;
+    private String author;
+
+    public Course(long id, String  name, String author) {
+c        this.id = id;
+        this.name = name;
+        this.author = author;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Course [id=" + id + ", name=" + name + ", author=" + author + "]";
+    }
+}
